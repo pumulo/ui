9df367d6097bc16c1689882636d5969687c84a32
Pumulo Sikaneta <pumulosikaneta@pumulos-mbp-2.lan>
2022-06-20 13:03:51 -0400

    initialize package on git

---
+++ dist/components/input.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Input = (props) => {
+    return (
+        <div>Input field</div>
+    )
+}
+
+export default Input;
---
+++ src/lib/components/button.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = (props) => {
+    return (
+        <div>Button field</div>
+    )
+}
+
+export default Button;
